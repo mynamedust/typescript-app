755eca418c04080f86612c3042c829705f9677f3
mynamedust <mynamedust@gmail.com>
2022-03-27 21:54:51 +0300

    upd

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -5,6 +5,7 @@ import cn from 'classnames';
 import { Header } from './Header/Header';
 import { Sidebar } from './Sidebar/Sidebar';
 import { Footer } from './Footer/Footer';
+import { AppContextProvider, IAppContext } from '../context/app.context';
 
 
 export const Layout = ({ children }: LayoutProps): JSX.Element => {
@@ -20,12 +21,14 @@ export const Layout = ({ children }: LayoutProps): JSX.Element => {
     );
 };
 
-export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FunctionComponent<T>) => {
     return function wuthLayoutComponent(props: T): JSX.Element {
         return (
-            <Layout>
-                <Component {...props} />
-            </Layout>
+            <AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
+                <Layout>
+                    <Component {...props} />
+                </Layout>
+            </AppContextProvider>
         );
     };
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,9 +20,7 @@ function Home({ menu }: HomeProps): JSX.Element {
       <Tag size='s' color='green'>маленький</Tag>
       <Tag size='m' color='primary'>маленький</Tag>
       <Rating rating={rating} isEditable setRating={setRating} />
-      <ul>
-        {menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
-      </ul>
+
     </>
 
   );
